214c346cd5fbc8b89892a8b2e44e729502b2ec19
makssMeinart <m.meinarts10@gmail.com>
2020-12-06 12:09:44 +0000

    First Project

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+$(function () {
+   $(`.header__slider`).slick({
+      fade: true,
+      arrows: true,
+      prevArrow: `<img class="slider-arrows slider-arrows__left" src="../Images/Header-Arrow-Left.svg" alt=""></img>`,
+      nextArrow: `<img class="slider-arrows slider-arrows__right" src="../Images/Header-Arrow-Right.svg" alt=""></img>`,
+      asNavFor: '.slider-dots'
+   });
+
+   $('.slider-dots').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      focusOnSelect: false,
+   });
+
+   $(".cards").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: `<img class="card__slider-arrow left" src="../Images/cardSlideArrowLeft.png"></img>`,
+      nextArrow: `<img class="card__slider-arrow right" src="../Images/cardSlideArrowRight.png"></img>`,
+   });
+
+})
